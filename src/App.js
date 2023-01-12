5f90ef889b4bd05d056d1e570ebfc4c6411e2d24
Gauravdeep Gosal <gauravdeep.gosal@capgemini.com>
2023-01-12 13:59:00 +0000

    changed message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Gauravdeep Gosal's website... to be completed</p>
         <a
           className="App-link"
           href="https://reactjs.org"
